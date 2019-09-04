e13e21e827a9f662e7b19e9a4a374517c68f418c
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-04 21:53:50 +0800

    fixed merged conflicts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,11 @@
 import * as admin from "firebase-admin"
 import authOnCreate from "./auth/onCreate";
 import usersOnUpdate from "./users/onUpdate";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import vehicleOnCreate from './vehicles/onCreate'
-import vehicleOnUpdate from './vehicles/onUpdate'
-import profileOnCreate from  "./profile/onCreate"
-=======
-import vehicleOnCreate from "./vehicles/onCreate"
-import vehicleOnUpdate from "./vehicles/onUpdate"
->>>>>>> bugfixes/general
-=======
 import vehicleOnCreate from "./vehicles/onCreate"
 import vehicleOnUpdate from "./vehicles/onUpdate"
 import profileOnCreate from  "./profile/onCreate"
 import profileOnUpdate from  "./profile/onUpdate"
->>>>>>> features/functions/profile
+
 
 admin.initializeApp();
 
