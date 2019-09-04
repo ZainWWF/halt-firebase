a7701cac76d08ab6f09fee718d5101f2722ce156
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-04 10:16:51 +0800

    added empty Maps to on creation of new User

--- functions/src/auth/onCreate.ts
+++ functions/src/auth/onCreate.ts
@@ -2,7 +2,11 @@ import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 
 /** create a new user document when a new firebase auth user is created */
-export default functions.auth.user()
+export default functions.region("asia-east2").auth.user()
 	.onCreate((user) => {
-		return admin.firestore().doc('users/' + user.uid).set({});
+		return admin.firestore().doc('users/' + user.uid).set({
+			vehicles : {},
+			plantations : {},
+			profile: {}
+		});
 	});
