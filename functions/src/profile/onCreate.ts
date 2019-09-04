4210166244fc7e21c68ab1cad84bedae1a188106
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-04 22:05:54 +0800

    added try/catch changed return error to string

--- functions/src/profile/onCreate.ts
+++ functions/src/profile/onCreate.ts
@@ -3,21 +3,31 @@ import * as admin from "firebase-admin"
 
 /** add entry into user's vehicle map when a vehicle doc is created*/
 export default functions.region("asia-east2").firestore
-	.document('profile/{userId}').onCreate(async(snap, context) => {
+	.document('profile/{userId}').onCreate(async (snap, context) => {
 
-		const userId = context.params.userId;
-		const newProfile = snap.data() as FirebaseFirestore.DocumentData;
-		const profileRef = `profiles/${userId}`
-		
-		await admin.firestore().doc(profileRef).update({
-			createdAt: admin.firestore.Timestamp.fromMillis(Date.now())
-		})
+		try {
+			const userId = context.params.userId;
+			const newProfile = snap.data() as FirebaseFirestore.DocumentData;
+			const profileRef = `profiles/${userId}`
 
-		return await admin.firestore().doc(`users/${userId}`).set({
-						profile: {
-							name : newProfile.name,
-							photoUrl : newProfile.photoUrl
-						}
-					}, { merge: true })
+			await admin.firestore().doc(profileRef).update({
+				createdAt: admin.firestore.Timestamp.fromMillis(Date.now())
+			})
+
+			await admin.firestore().doc(`users/${userId}`).set({
+				profile: {
+					name: newProfile.name,
+					photoUrl: newProfile.photoUrl
+				}
+			}, { merge: true })
+
+			return
+
+		} catch (error) {
+
+			return "Error: " + error
+
+		}
+
 
 	})
