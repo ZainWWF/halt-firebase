64a24f087aa82f7f1370213969e38de0f9820eb5
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-04 20:25:33 +0800

    updated to async/await removed unnecessary code

--- functions/src/vehicles/onUpdate.ts
+++ functions/src/vehicles/onUpdate.ts
@@ -3,43 +3,35 @@ import * as admin from "firebase-admin"
 
 /** update User.vehicle map entry when the vehicle document is updated */
 export default functions.region("asia-east2").firestore
-	.document('vehicles/{vehicleId}').onUpdate((change, context) => {
+	.document('vehicles/{vehicleId}').onUpdate(async (change, context) => {
 
-		const getPropValues = (data: any) => {
-			const { chassis, colour, license, loadingCapacity, make, url } = data;
-			return [chassis, colour, license, loadingCapacity, make, url]
+		const newVehicleData = change.after.data() as FirebaseFirestore.DocumentData;;
+		const vehicleRef = 'vehicles/' + context.params.vehicleId
+
+		try {
+
+			await admin.firestore()
+				.doc(vehicleRef)
+				.update({
+					updatedAt: admin.firestore.Timestamp.fromMillis(Date.now())
+				})
+
+			await admin.firestore().doc('users/' + newVehicleData.userId).set({
+				vehicles: {
+					[context.params.vehicleId]: {
+						ref: admin.firestore().doc(vehicleRef),
+						license: newVehicleData.license,
+						make: newVehicleData.make,
+						url: newVehicleData.url
+					}
+				}
+			}, { merge: true })
+
+			return 
+
+		} catch (error) {
+
+			return Error
 		}
 
-		const previousVehicleData = change.before.data() as FirebaseFirestore.DocumentData;;
-		const newVehicleData = change.after.data() as FirebaseFirestore.DocumentData;;
-
-		const previousVehiclePropValues = getPropValues(previousVehicleData);
-		const newVehiclePropValues = getPropValues(newVehicleData);
-
-		const changedPropValues = previousVehiclePropValues.filter(oldPropValue => {
-			return !newVehiclePropValues.some((newPropValue) => {
-				return oldPropValue === newPropValue
-			})
-		})
-
-		/** no change was found so return null */
-		if (changedPropValues.length === 0) return null;
-
-		const vehicleRef = 'vehicles/' + context.params.vehicleId
-		return admin.firestore().doc(vehicleRef)
-			.update({
-				updatedAt: admin.firestore.Timestamp.fromMillis(Date.now())
-			})
-			.then(() => {
-				return admin.firestore().doc('users/' + newVehicleData.userId).set({
-					vehicles: {
-						[context.params.vehicleId]: {
-							ref: admin.firestore().doc(vehicleRef),
-							license: newVehicleData.license,
-							make: newVehicleData.make,
-							url: newVehicleData.url
-						}
-					}
-				}, { merge: true })
-			})
 	});
