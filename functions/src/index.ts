6167745eaa0b3dfbf15dd91f0ea15a4f2ef0bbff
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-04 13:17:05 +0800

    code cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as admin from "firebase-admin"
 import authOnCreate from "./auth/onCreate";
 import usersOnUpdate from "./users/onUpdate";
-import vehicleOnCreate from './vehicles/onCreate'
-import vehicleOnUpdate from './vehicles/onUpdate'
+import vehicleOnCreate from "./vehicles/onCreate"
+import vehicleOnUpdate from "./vehicles/onUpdate"
 
 admin.initializeApp();
 
