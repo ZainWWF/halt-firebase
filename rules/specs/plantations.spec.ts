52b45638b547941fff81dc79909af51292c0b3ff
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-09-09 15:12:42 +0800

    added rules for plantations

---
+++ rules/specs/plantations.spec.ts
@@ -0,0 +1,66 @@
+
+import { expectExtend, setup, teardown } from "./helpers";
+import { firestore } from "firebase";
+
+describe('Plantations rules', () => {
+	let db: firestore.Firestore
+	let ref: firestore.CollectionReference;
+
+	afterAll(async () => {
+		await teardown();
+	})
+
+	test('deny read/write when not unauthenticated', async () => {
+		db = await setup(null, null);
+		ref = db.collection('plantations');
+		await expectExtend(ref.get()).toDeny()
+		await expectExtend(ref.add({})).toDeny()
+	})
+
+	test('allow only Owner read', async () => {
+		db = await setup({ uid: "john123" }, {
+			"plantations/v456" :{
+				userId: "john123"
+			}
+		});
+		ref = db.collection('plantations');
+		await expectExtend(ref.doc("v456").get()).toAllow()
+		await expectExtend(ref.doc("v123").get()).toDeny()
+	})
+
+	test('allow Owner create when authenticated', async () => {
+		db = await setup({ uid: "john123" }, {});
+		ref = db.collection('plantations')
+		await expectExtend(ref.doc("v456").set(
+			{
+				userId : "john123"
+			}
+		)).toAllow()
+	})
+
+
+	test('only allow Owner to update all except userId when authenticated', async () => {
+		db = await setup({ uid: "mike456" }, {
+			"plantations/v123" : {
+				userId: "mike456",
+				colour: "blue"
+			}
+		});
+		ref = db.collection('plantations');
+		await expectExtend(ref.doc("v123").update({
+			userId: "mike456",
+			name: "red"
+		})).toAllow()
+		await expectExtend(ref.doc("v123").update({
+			userId: "joe123",
+			name: "red"
+		})).toDeny()
+	})
+
+
+	test('deny delete when authenticated', async () => {
+		db = await setup({ uid: "john123" }, {});
+		ref = db.collection('plantations')
+		await expectExtend(ref.doc().delete()).toDeny()
+	})
+})
