a5f2aad3635055f969dc501e4cdf19651e03cd56
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-10-05 22:14:24 +0800

    fixed doc path error

--- functions/src/assistance/onCreate.ts
+++ functions/src/assistance/onCreate.ts
@@ -3,11 +3,11 @@ import * as admin from "firebase-admin"
 
 /** add entry into user's assistance map when a assistance doc is created*/
 export default functions.region("asia-east2").firestore
-	.document('assistances/{assistanceId}').onCreate(async (snap, context) => {
+	.document('assistance/{assistanceId}').onCreate(async (snap, context) => {
 
 		try {
 
-			const assistanceRef = 'assistances/' + context.params.assistanceId
+			const assistanceRef = 'assistance/' + context.params.assistanceId
 
 			await admin.firestore().doc(assistanceRef)
 				.update({
