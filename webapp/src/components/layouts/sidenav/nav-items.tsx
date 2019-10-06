303878d6760ac047b598b17889672efb5da5aaa5
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-10-06 11:27:17 +0800

    added mills to sidenav and parent folder name

--- webapp/src/components/layouts/sidenav/nav-items.tsx
+++ webapp/src/components/layouts/sidenav/nav-items.tsx
@@ -1,29 +1,50 @@
 import React, { forwardRef } from "react";
 import WorkIcon from "@material-ui/icons/Work";
+import ViewListIcon from "@material-ui/icons/ViewList";
 import { Link, LinkProps } from "react-router-dom";
 import AssetsTopBar from "../header/assets/AssetsTopBar";
 import { Omit } from '@material-ui/types';
-import AssetsContents from "../contents/assets/AssetsContents";
+import AssetsContents from "../contents/trader/assets/AssetsContents";
+
+
 
 const navItems = [
-  {
-    id: "",
-    group: [],
-    children: [
-      {
-        id: "Assets",
-        group: [],
-        icon: <WorkIcon />,
-        path: "/assets",
+	{
+		id: "Trader",
+		group: [],
+		children: [
+			{
+				id: "Assets",
+				group: [],
+				icon: <WorkIcon />,
+				path: "/assets",
 				link: forwardRef<HTMLAnchorElement, Omit<LinkProps, 'innerRef' | 'to'>>(
 					(props, ref) => <Link innerRef={ref as any} to="/assets/" {...props} />,
 				),
-        content: <AssetsContents key="assets-contents"/>,
-        header: () => <AssetsTopBar />
+				content: <AssetsContents key="assets-contents" />,
+				header: () => <AssetsTopBar />
 			},
 
-    ]
-  }
+		]
+	},
+	{
+		id: "Mills",
+		group: [],
+		children: [
+			{
+				id: "Directory",
+				group: [],
+				icon: <ViewListIcon />,
+				path: "/mills",
+				link: forwardRef<HTMLAnchorElement, Omit<LinkProps, 'innerRef' | 'to'>>(
+					(props, ref) => <Link innerRef={ref as any} to="/mills/" {...props} />,
+				),
+				content: <div key="mills-contents" />,
+				header: () => <div />
+			},
+
+		]
+	}
 ];
 
 export default navItems;
