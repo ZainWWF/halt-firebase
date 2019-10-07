6e5698926c91322f060d1fdda6f479d9d6eb6fae
Mohamad Zain Aziz <zaziz@wwf.sg>
2019-10-07 15:12:30 +0800

    add content module for directory

---
+++ webapp/src/components/layouts/contents/mills/directory/DirectoryContents.tsx
@@ -0,0 +1,54 @@
+
+import React, { useContext, useEffect, useState, memo } from 'react';
+import { Switch, Route, Redirect, } from 'react-router-dom';
+import { FirebaseContext, Firebase } from '../../../../providers/Firebase/FirebaseProvider';
+import ListingView from "./listing/ListingView"
+import RepsView from "./reps/RepsView"
+
+const Contents = memo(() => {
+
+	const firebaseApp = useContext(FirebaseContext) as Firebase;
+	const [mills, setMills] = useState()
+	const [isRetrieving, setIsRetrieving] = useState(true)
+
+	useEffect((
+	) => {
+		let isSubscribed = true
+		firebaseApp.db.collection("mills")
+			.get()
+			.then((data) => {
+				if (isSubscribed) {
+					console.log("retrieve done")
+					const mills = data.docs.map(snap => {
+						return snap.data()
+					})
+					setMills(mills)
+					setIsRetrieving(false)
+				}
+
+			}).catch((error: Error) => {
+				console.error(error)
+			})
+		return () => { isSubscribed = false }
+	}, [])
+
+	return (
+		<Switch>
+			<Redirect exact from="/directory" to={"/directory/listing"} />
+
+			<Route
+				path="/directory/listing"
+				component={() => <ListingView mills={mills} isRetrieving={isRetrieving} />}
+			/>
+
+			<Route
+				path="/directory/reps"
+				component={RepsView}
+			/>
+		</Switch>
+	)
+
+});
+
+
+export default Contents;
